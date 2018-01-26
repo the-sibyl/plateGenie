3c456e2e406e144d42c7317477ea8d83c20e3776
User U. User <user@user.user>
2018-01-26 06:46:23 +0000

    I wrote code for trapezoidal acceleration profiles. I might rewrite some of it to precalculate values better and not panic when bad parameters are given.

--- main.go
+++ main.go
@@ -32,7 +32,14 @@ import (
 )
 
 const (
-	stepperSpeed = 5.0
+	// Stepper speed in microseconds
+	stepperSpeed = 1000
+	// Acceleration/deceleration window in microseconds for trapezoidal acceleration profile
+	trapAccelPeriod = 50000
+	// Minimum delay for the trapezoidal profile: this may be zero (fastest)
+	trapMinimumDelay = 50
+	// Maximum delay for the trapezoidal profile: this must not exceed the acceleration period
+	trapMaximumDelay = 1000
 	// The plater has a 500mm 4-start leadscrew with a 2mm pitch and 8mm lead.
 	screwLead   = 8.0
 	stepsPerRev = 200.0
@@ -42,8 +49,6 @@ const (
 
 // Distance is in millimeters. The sign connotes direction.
 func move(s *softStepper.Stepper, dist int) {
-	fmt.Println(math.Abs(float64(dist)))
-	fmt.Println(float64(maxDistance))
 	if math.Abs(float64(dist)) > float64(maxDistance) {
 		panic("Requested distance exceeds safe travel limits.")
 	}
@@ -51,10 +56,76 @@ func move(s *softStepper.Stepper, dist int) {
 	numSteps := int(math.Floor(math.Abs((float64(dist))/screwLead) * stepsPerRev))
 
 	if dist < 0 {
-		s.StepForwardMulti(numSteps)
+		s.StepForwardMulti(-numSteps)
 	} else if dist > 0 {
 		s.StepBackwardMulti(numSteps)
 	}
+}
+
+// Move with a trapezoidal acceleration profile
+func moveTrapezoidal(s *softStepper.Stepper, dist int) {
+	if math.Abs(float64(dist)) > float64(maxDistance) {
+		panic("Requested distance exceeds safe travel limits.")
+	}
+
+	averageDelay := float64(trapMaximumDelay + trapMinimumDelay) / 2.0
+	numDelayDivisions := int(math.Floor(trapAccelPeriod / averageDelay))
+	delayIncrement := float64(trapMaximumDelay - trapMinimumDelay) / float64(numDelayDivisions)
+
+	fmt.Println(averageDelay, numDelayDivisions, delayIncrement)
+
+
+	// Accelerate
+	stepCountAcc := 0
+	for delay := float64(trapMaximumDelay); delay >= float64(trapMinimumDelay); delay = delay - delayIncrement {
+		if stepCountAcc > numDelayDivisions {
+			panic("Trapezoidal acceleration error")
+		}
+		if dist < 0 {
+			s.StepForward()
+		} else if dist > 0 {
+			s.StepBackward()
+		}
+		time.Sleep(time.Microsecond * time.Duration(delay))
+		stepCountAcc++
+	}
+
+	// Constant speed
+
+	numStepsConstantSpeed := int(math.Floor(math.Abs((float64(dist))/screwLead) * stepsPerRev)) - 2 * stepCountAcc
+
+	// FIXME: Enforce that the acceleration and deceleration segments do not cause the carriage to travel too far!
+	if numStepsConstantSpeed < 0 {
+		panic("Acceleration/deceleration profile is too long")
+	}
+
+	if dist < 0 {
+		s.StepForwardMulti(numStepsConstantSpeed)
+	} else if dist > 0 {
+		s.StepBackwardMulti(numStepsConstantSpeed)
+	}
+
+	// Decelerate
+	stepCountDec := 0
+	for delay := float64(trapMinimumDelay); delay <= float64(trapMaximumDelay); delay = delay + delayIncrement {
+		if stepCountDec > numDelayDivisions {
+			panic("Trapezoidal acceleration error")
+		}
+		if dist < 0 {
+			s.StepForward()
+		} else if dist > 0 {
+			s.StepBackward()
+		}
+		time.Sleep(time.Microsecond * time.Duration(delay))
+		stepCountDec++
+	}
+	fmt.Println("Accel Steps:", stepCountAcc)
+	fmt.Println("Decel Steps:", stepCountDec)
+	if stepCountAcc != stepCountDec {
+		panic(`Mismatch between acceleration and deceleration steps. 
+			This would cause the carriage to drift in one direction.`)
+	}
+
 }
 
 // Add a homing function with a timeout
@@ -69,16 +140,19 @@ func move(s *softStepper.Stepper, dist int) {
 //	Agitation timer
 
 func main() {
-	stepper1 := softStepper.InitStepper(18, 23, 24, 25, 8, time.Microsecond*1000)
+	stepper1 := softStepper.InitStepper(18, 23, 24, 25, 8, time.Microsecond * stepperSpeed)
 	defer stepper1.ReleaseStepper()
 
 	stepper1.EnableHold()
 
-	move(stepper1, 5)
 
 	for {
+		moveTrapezoidal(stepper1, 10)
+		moveTrapezoidal(stepper1, -10)
+/*
 		move(stepper1, -10)
 		move(stepper1, 10)
+*/
 	}
 
 }
